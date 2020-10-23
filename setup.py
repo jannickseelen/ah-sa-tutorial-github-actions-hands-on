aa432f0310dbf2270a94007cd3eb38d28ebe6756
Jannick Seelen <jannick.seelen@ah.nl>
2020-10-23 11:02:20 +0200

    Added pre-commit yaml

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='sample_project',
+    name="sample_project",
     packages=find_packages(),
-    version='0.1.0',
-    description='Hands-on session on Github Actions',
-    author='J. Offenberg',
+    version="0.1.0",
+    description="Hands-on session on Github Actions",
+    author="J. Seelen",
     install_requires=[],
-    extras_require={
-        "dev": [
-            "pytest",
-            "pre-commit==2.7.1"
-        ]
-    }
+    extras_require={"dev": ["pytest", "pre-commit==2.7.1"]},
 )
